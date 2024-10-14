8a9fb8b2ba6f24617d5f1b60b1c88576deaaa7d3
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-10-14 15:34:38 +0200

    enable cors

--- backend/blog-api/index.js
+++ backend/blog-api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const postsRoute = require('./routes/posts');
+const cors = require('cors');
+
 
 const app = express();
 const port = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
